427d2534ad760588e81ae69158775451487cd263
Raffaello Di Martino IZ0QWM <42165424+iz0qwm@users.noreply.github.com>
2018-09-08 09:25:22 +0200

    First version

---
+++ arduipometeo.ino
@@ -0,0 +1,191 @@
+/* Stazione meteo ipogea
+   Raffaello Di Martino IZ0QWM
+   http://www.kwos.it */
+
+//Libraries
+#include <DHT.h>
+#include "RTClib.h"
+#include <DS3231.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+#include <stdio.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+
+//Costanti generiche
+int i = 0;
+
+//Costanti per termometro
+#define DHTPIN 7     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+//Costanti per anemometro
+#define PIN_ANEMOMETER 3 // Digital 3
+// Tempo di aggiornamento della velocit� del vento
+#define MSECS_CALC_WIND_SPEED 5000
+
+//Variabili per anemometro
+volatile int numRevsAnemometer = 0; // Incremented in the interrupt
+
+File logfile;
+
+//Start Modulo RTC 
+DS3231  rtca(SDA, SCL); //Solo per leggere
+RTC_DS3231 rtc; //Make a RTC DS3231 object per settare l'orario
+
+//Start modulo BMP280
+Adafruit_BMP280 bme; // I2C
+
+void setup()
+{
+  
+  Serial.begin(115200);
+
+  //Anemometro
+  pinMode(PIN_ANEMOMETER, INPUT);
+  digitalWrite(PIN_ANEMOMETER, HIGH);
+  attachInterrupt(1, countAnemometer, FALLING);
+  // Realtime Clock
+  rtca.begin(); // Solo per leggere
+  rtc.begin(); // Solo per settare l'orario
+  // Sensore pressione
+  bme.begin();
+  //Serial.print("---------------------------------------------------\n");
+  //Serial.print("INIZIALIZZAZIONE\n");
+  //Serial.print("---------------------------------------------------\n");
+  //Serial.print("Setto l'orario per il log file...\n");
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //Prendiamo l'orario dal PC
+
+  //SD Card
+  //Serial.print("Inizializzazione SD Card...\n");
+
+  if (!SD.begin(4)) {// Usiamo il pin 4 dove c'� il CS per capire se la scheda � inserita
+    //Serial.println("Inizializzazione fallita!\n");
+    while (1);
+  }
+  //Serial.println("Inizializzazione OK.\n");  
+  
+}
+
+void loop()
+{
+    // create a new file
+    char filename[] = "LOGGER00.CSV";
+    for (uint8_t i = 0; i < 100; i++) {
+      filename[6] = i/10 + '0';
+      filename[7] = i%10 + '0';
+      if (! SD.exists(filename)) {
+        //logfile.close();
+        logfile = SD.open(filename, FILE_WRITE); 
+        // Serial.println("GIORNO,ORA,HUM,TEMPOUT,TEMPINT,WIND,TEMPBARO,PRESS");
+        logfile.println("GIORNO,ORA,HUM,TEMPOUT,TEMPINT,WIND,TEMPBARO,PRESS\n");
+        logfile.close();
+        delay(500);
+      }
+      logfile = SD.open(filename, FILE_WRITE);
+      delay(500); 
+      //Serial.println(logfile.size());
+      if (logfile.size() < 200000) {
+        break;  // leave the loop!
+      }
+    } 
+    delay(1000);
+    String DataOra = dataora();
+    String TempHum = calcTempHumidity();
+    //Prendo la stringa dalla funzione WindSpeed
+    String Wind = calcWindSpeed();
+    //Prendo la stringa dalla funzione calcPressure
+    String Pressure = calcPressure();
+    //Creo la stringa completa
+    String total = DataOra + "," + TempHum + "," + Wind + "," + Pressure;
+    
+    logfile.println(total);
+    //Serial.println(total);
+    logfile.close();
+    
+    // Azzero le rivoluzioni dell'anemometro
+    numRevsAnemometer = 0;
+    
+    delay(5000); //Delay 5 sec.
+
+}
+
+//=======================================================
+// Interrupt handler for anemometer. Called each time the reed
+// switch triggers (one revolution).
+//=======================================================
+void countAnemometer() {
+numRevsAnemometer++;
+}
+
+String dataora() {
+    //Leggi ora e data
+    String data = rtca.getDateStr();
+    String separatore = ",";
+    String ora = rtca.getTimeStr();
+    String data_ora = data + separatore + ora;
+    //Serial.println(data_ora);
+    return data_ora;
+}
+String calcTempHumidity() {
+    //Variabili per termometro
+    float hum;  //Valore umidit� DHT
+    float tempOut; //Valore temperatura DHT
+    float tempIn;//Valore temperatura DS3231
+    dht.begin();
+    tempIn = rtca.getTemp();
+    //Leggi i dati di temperatura e umidit� e metti nelle variabili
+    hum = dht.readHumidity();
+    tempOut = dht.readTemperature();
+    String separatore = ",";
+    String hum_temp = hum + separatore + tempOut + separatore + tempIn;
+    //Serial.println(hum_temp);
+    return hum_temp;
+  
+}
+
+
+String calcPressure() {
+    float tempBmp;
+    tempBmp = bme.readTemperature();
+    //Serial.print(bme.readTemperature());
+    float pascal, hpa;
+    pascal = bme.readPressure();
+    //Serial.print(bme.readPressure());
+    hpa = pascal * 0.01;
+    
+    String separatore = ",";
+    //Serial.print(meters);
+    String temp_press = tempBmp + separatore + hpa;
+    //Serial.print(pascal);
+    return temp_press;   
+} 
+
+//=======================================================
+// Calculate the wind speed, and send to serial port.
+// 1 rev/sec = 2.5 mph
+//=======================================================
+String calcWindSpeed() {
+int y, x, iSpeed;
+// This will produce mph * 10
+// (didn't calc right when done as one statement)
+long speed = 25000;//2.5 mph * 1000
+speed *= numRevsAnemometer;
+speed /= MSECS_CALC_WIND_SPEED;
+iSpeed = speed; // Need this for formatting below
+
+x = iSpeed / 10;
+y = x * 1.60934;
+String punto = ".";
+//Serial.print('.');
+x = iSpeed % 10;
+String wind = y + punto + x;
+return wind;
+//Serial.println(wind);
+numRevsAnemometer = 0; // Reset counter
+}
+
+   
